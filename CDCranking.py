120ffc16d9aa20b6e0a3cda683f32ada133d612c
Nick Rumbaugh <takkyon13@gmail.com>
2015-12-08 22:19:21 -0800

    Deal with specific deleted post

--- CDCranking.py
+++ CDCranking.py
@@ -10,6 +10,7 @@ def CDCrank(pagefiles,outfile=None,disqual_flags=None,top=20):
     disqual_flags[0][0]=0
     vote_arr,names_arr,score_arr=np.zeros((len(pagefiles),20),dtype='i8'),np.zeros((len(pagefiles),20),dtype='|S100'),np.zeros((len(pagefiles),20),dtype='f8')
     avgs_arr=np.zeros(len(pagefiles))
+    #vote_arr,names_arr=vote_arr.flatten(),names_arr.flatten()
     for page in range(0,len(pagefiles)):
         curpage=open(pagefiles[page]).read()
         finddecks_temp1=curpage.split('<span class="html-attribute-name">data-rating-form-type</span>="<span class="html-attribute-value">up-down')
@@ -26,12 +27,35 @@ def CDCrank(pagefiles,outfile=None,disqual_flags=None,top=20):
         for i in range(0,pagelen):
             temparr=findnames_temp1[i+1].split('</a>')
             pagenames=np.append(pagenames,temparr[0])
+        if page==27: 
+            pagenames=np.insert(pagenames,7,'DELETED')
+            lastname=pagenames[-1]
+            pagenames=pagenames[:-1]
+        if page==28:
+            pagenames=np.append(lastname,pagenames)
+            lastname=pagenames[-1]
+            pagenames=pagenames[:-1]
+        if page==29:
+            pagenames=np.append(lastname,pagenames)
+            lastname=pagenames[-1]
         findvotes_temp1=curpage.split('<span class="html-attribute-name">data-rating-sum</span>="<span class="html-attribute-value">')
         if len(delinds)>0:findvotes_temp1=np.delete(findvotes_temp1,delinds)
         pagevotes=np.zeros(0)
         for i in range(0,pagelen):
             temparr=findvotes_temp1[i+1].split('</span>')
             pagevotes=np.append(pagevotes,int(temparr[0])-1)
+        if page==27: 
+            pagevotes=np.insert(pagevotes,7,0)
+            lastvote=pagevotes[-1]
+            pagevotes=pagevotes[:-1]
+        if page==28:
+            pagevotes=np.append(lastvote,pagevotes)
+            lastvote=pagevotes[-1]
+            pagevotes=pagevotes[:-1]
+        if page==29:
+            pagevotes=np.append(lastvote,pagevotes)
+            lastvote=pagevotes[-1]
+            pagelen+=1
         vote_arr[page][:pagelen],names_arr[page][:pagelen]=pagevotes,pagenames
         if page==len(pagefiles)-1: 
             disqual_flags[page][pagelen-1:]=0
@@ -47,5 +71,8 @@ def CDCrank(pagefiles,outfile=None,disqual_flags=None,top=20):
     print 'Rank           Name                 Page Post Votes Score Vote Avg.'
     for i in range(0,top):
         print '%4i %30s %4i %4i %4i %7.3f %7.3f'%(i+1,names_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],1+(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20,(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])%20,vote_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],score_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],avgs_arr[(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20])
-        if outfile != None: FILE.write('%4i %30s %4i %4i $4i %7.3 %7.3\n'%(i+1,names_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],1+(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20,(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])%20,vote_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],score_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],avgs_arr[(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20]))
+        if outfile != None: FILE.write('%4i %30s %4i %4i %4i %7.3 %7.3\n'%(i+1,names_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],1+(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20,(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])%20,vote_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],score_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],avgs_arr[(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20]))
+    if outfile != None:
+        for i in range(top,np.sum(disqual_flags)):
+            FILE.write('%4i %30s %4i %4i %4i %7.3 %7.3\n'%(i+1,names_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],1+(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20,(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])%20,vote_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],score_arr.flatten()[disqual_flags.flatten()][sort_inds[i]],avgs_arr[(1+np.arange(20*len(pagefiles))[disqual_flags.flatten()][sort_inds[i]])/20]))
     if outfile != None:FILE.close()
